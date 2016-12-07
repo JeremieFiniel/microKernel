60ea9ff69284827bb13ae65b100776b3579b77c3
JeremieFiniel <jeremiefiniel@gmail.com>
2016-12-07 17:40:52 +0100

    creating devices.h

---
+++ devices.h
@@ -0,0 +1,14 @@
+#ifndef DEVICES_H_
+#define DEVICES_H_
+
+void init_uart_device();
+void top_uart();
+void bottom_uart();
+
+struct Bottom_event{
+	irq_id_t irq;
+	void (*bottom_func)(void *);
+	struct Bottom_event *next;
+};
+
+#endif
