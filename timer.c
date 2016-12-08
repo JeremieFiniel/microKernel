069c86d0434880e2b184ce0fc96d39522e4ab437
JeremieFiniel <jeremiefiniel@gmail.com>
2016-12-08 05:31:07 +0100

    impelmentation of the timer driver

--- timer.c
+++ timer.c
@@ -1,4 +1,43 @@
 #include "timer.h"
 #include "board.h"
 
-extern uint32_t periph_base;
+static uint32_t periph_base = 0x0;
+
+void cortex_a9_timer_init()
+{
+	periph_base = cortex_a9_peripheral_base();
+}
+
+void cortex_a9_timer_enable_irq()
+{
+	uint32_t *control_register = periph_base + ARM_PWT_BASE_OFFSET + ARM_TIMER_CR;
+	*control_register |= ARM_TIMER_CR_TE;
+}
+
+void cortex_a9_timer_disable_irq()
+{
+	uint32_t *control_register = periph_base + ARM_PWT_BASE_OFFSET + ARM_TIMER_CR;
+	*control_register &= ~(ARM_TIMER_CR_TE);
+}
+
+void cortex_a9_timer_clear_irq()
+{
+	uint32_t *interrupt_register = periph_base + ARM_PWT_BASE_OFFSET + ARM_TIMER_ISR;
+	// The event flag is cleared when written to 1.
+	*interrupt_register = 1;
+}
+
+void cortex_a9_timer_set_timer(int t)
+{
+	uint32_t *load_register = periph_base + ARM_PWT_BASE_OFFSET + ARM_TIMER_LR;
+	*load_register = t;
+}
+
+void cortex_a9_timer_set_auto_load(int s)
+{
+	uint32_t *control_register = periph_base + ARM_PWT_BASE_OFFSET + ARM_TIMER_CR;
+	if (s)
+		*control_register |= ARM_TIMER_CR_AR;
+	else
+		*control_register &= ~(ARM_TIMER_CR_AR);
+}
--- timer.h
+++ timer.h
@@ -1,6 +1,26 @@
 #ifndef TIMER_H_
 #define TIMER_H_
 
+/*
+ * Cortex_a9 MPCore, Thechnical Reference Manual
+ * 4.2 Private timer and watchdog registers (page 64)
+ */
 
+#define ARM_TIMER_LR 0x0000		// load register
+#define ARM_TIMER_COUR 0x0004	// counter register
+#define ARM_TIMER_CR 0x0008 	// control register
+#define ARM_TIMER_ISR 0x000C 	// interrupt state register
+
+#define ARM_TIMER_CR_TE (1<<0)	// time enable
+#define ARM_TIMER_CR_AR (1<<1)	// auto reload
+#define ARM_TIMER_CR_IE (1<<1)	// irq enable
+
+void cortex_a9_timer_init();
+
+void cortex_a9_timer_enable_irq();
+void cortex_a9_timer_disable_irq();
+void cortex_a9_timer_clear_irq();
+void cortex_a9_timer_set_timer(int t);
+void cortex_a9_timer_set_auto_load(int s);
 
 #endif
